c2cd14c22e61a412585e632452dda6a126408cd0
suras <suras@DESKTOP-LKR6MBR.attlocal.net>
2020-04-14 20:45:25 -0500

    Practice

---
+++ Practice/src/inventory.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//import java.util.Random;
+
+public class inventory {
+
+	/*public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		System.out.println("name");
+		String name = x.next();
+		System.out.println("Hello " + name);**/
+		
+		
+		
+		
+		/*Scanner x = new Scanner(System.in);
+		System.out.println("Enter number");
+		int a = x.nextInt();
+		System.out.println("Enter another number");
+		int b = x.nextInt();
+		int Total = a + b;
+		System.out.println(Total);**/
+		
+		
+		
+	
+	 /* Random r = new Random();
+	 int x = r.nextInt(6)+1;
+	 System.out.println(x);**/
+	}
+}
